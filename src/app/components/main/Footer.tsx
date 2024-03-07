bcea054fd54b000ea745f0265a89cde40d9fd7e2
lst97 <laisiotou1997@gmail.com>
2024-03-07 17:58:26 +1100

    update: add simple chrome dino game

--- src/app/components/main/Footer.tsx
+++ src/app/components/main/Footer.tsx
@@ -1,21 +1,45 @@
+import { PkmLink } from "../common/Links";
 import { PixelContainer } from "../common/layout/Containers";
-import styles from "./footer.module.css";
+import ChromeDinoGame from "../utils/ChromeGames";
 
 export function Footer({
-  children,
   className,
 }: {
   children?: React.ReactNode;
   className?: string;
 }) {
   return (
-    <PixelContainer borderTextTop="Footer" className={`${className}`}>
-      {children}
+    <PixelContainer className={`${className} bg-amber-100 p-4`}>
+      <div className="flex flex-row space-x-4 w-full justify-between">
+        <div className="flex flex-col">
+          <div className="flex flex-row space-x-4 basis-3/4">
+            <PkmLink href="#">
+              <p className="inline">Home</p>
+            </PkmLink>
+            <PkmLink href="#">
+              <p className="inline">Resources</p>
+            </PkmLink>
+            <PkmLink href="#">
+              <p className="inline">Notes</p>
+            </PkmLink>
+            <PkmLink href="#">
+              <p className="inline">Software</p>
+            </PkmLink>
+            <PkmLink href="#">
+              <p className="inline">About</p>
+            </PkmLink>
+          </div>
+          <div>Guest Book</div>
+          <div>Email</div>
+        </div>
+        <div className="flex flex-col basis-1/4">Right</div>
+      </div>
       <footer>
         <div>
           <p>© 2024 lst97.dev</p>
         </div>
       </footer>
+      <ChromeDinoGame />
     </PixelContainer>
   );
 }
